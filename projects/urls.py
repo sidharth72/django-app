6538b09b0f522f2f91a7d051ce7592405f6379d5
sidharth72 <sidharthan261@gmail.com>
2021-05-16 03:38:16 -0700

    some changes added

--- projects/urls.py
+++ projects/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-admin-what-is-the-true-nature-of-the-universe/', admin.site.urls),
     path('',include("blogapp.urls")),
 
 
