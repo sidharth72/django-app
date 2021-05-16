d69c7a29d382e2337d4e392c6572ae5e6fe34f27
sidharth72 <sidharthan261@gmail.com>
2021-05-16 02:09:25 -0700

    ready to deploy

--- projects/settings.py
+++ projects/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -191,4 +192,4 @@ TINYMCE_DEFAULT_CONFIG = {
     'statusbar': True,
 }
 
-
+django_heroku.settings(locals())
